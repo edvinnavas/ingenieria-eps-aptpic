b38249727576de7ec9a6b1c63872bffa17bc0ef5
Edvin Navas <edvin.navas@gmail.com>
2022-01-21 01:44:08 -0600

    Solicitud Promoción Docente y Solicitud Licencia Docente.
    Se actualizó la Solicitud Promoción Docente.
    Se actualizó la Solicitud Licencia Docente.

--- db-comeval/SQL COMEVAL.sql
+++ db-comeval/SQL COMEVAL.sql
@@ -44,7 +44,7 @@ SELECT swh.id_solicitud, swh.id_workflow ,es.nombre estado_solicitud, ts.nombre 
 FROM solicitud_workflow_historial swh 
 	 LEFT JOIN estado_solicitud_comeval es ON (swh.id_estado_solicitud = es.id_estado_solicitud AND swh.id_tipo_solicitud = es.id_tipo_solicitud) 
 	 LEFT JOIN tipo_solicitud_comeval ts ON (es.id_tipo_solicitud = ts.id_tipo_solicitud)
-WHERE swh.id_tipo_solicitud = 2 AND swh.id_solicitud = 1
+WHERE swh.id_tipo_solicitud = 2 -- AND swh.id_solicitud = 1
 ORDER BY swh.id_tipo_solicitud, swh.id_solicitud,  swh.id_workflow, swh.id_estado_solicitud;
 
 -- CODIGO DE PERSONAL PARA PRUEBAS.
@@ -54,8 +54,10 @@ SELECT d.* FROM personal d WHERE d.nombre ILIKE '%david estuardo%' AND d.apellid
 -- 000013564, 000017976, 000940264
 
 -- PLAZA PERSONAL.
-SELECT p.* FROM plazapuesto p WHERE p.puesto='210121';
-SELECT p.* FROM plazapersonal p WHERE p.personal = '000018150' ORDER BY p.anio, p.periodo;
+SELECT p.* FROM plazapersonal p WHERE p.anio=2021 and p.periodo=1;
+SELECT p.* FROM plazapersonal p WHERE p.anio=2021 and p.periodo=1 and p.personal='019990748';
+SELECT p.* FROM plazapuesto p WHERE p.puesto='210141';
+SELECT p.* FROM plazapartida p WHERE p.puesto='210121';
 
 -- PLAZAPARTIDA.
 SELECT p.* FROM plazapartida p WHERE (p.plaza, p.subpartida, p.renglon) IN (SELECT pp.plaza, pp.subpartida, pp.renglon 
@@ -66,7 +68,7 @@ SELECT ppp.* FROM plaza ppp WHERE ppp.plaza IN (SELECT p.plaza
                                                 FROM plazapartida p 
                                                 WHERE (p.plaza, p.subpartida, p.renglon) IN (SELECT pp.plaza, pp.subpartida, pp.renglon 
                                                                                              FROM plazapersonal pp 
-                                                                                             WHERE pp.personal = '000018150'));
+                                                                                             WHERE pp.personal = '019990748'));
 -- SUBPARTIDA POR PERSONAL.
 SELECT s.* FROM subpartida s WHERE s.subpartida IN (SELECT p.subpartida 
                                                     FROM plazapartida p 
@@ -75,33 +77,8 @@ SELECT s.* FROM subpartida s WHERE s.subpartida IN (SELECT p.subpartida
                                                                                                  WHERE pp.personal = '000018150'));
 
 -- ========= TITULARIDADHISTORIAL Y TITULARIDAD. =========
-SELECT th.* FROM titularidadhistorial th WHERE th.personal='000930066' ORDER BY fechaascenso;
+SELECT th.* FROM titularidadhistorial th WHERE th.personal='000930131' ORDER BY fechaascenso;
 SELECT t.* FROM titularidad t WHERE t.personal='000930066';
--- INSERT INTO titularidad (personal, puesto, acta, fechasistema, titular, dictamen, dictamenemisor) VALUES ('000017976','210170',1,'2021-10-01 17:38:02.728', true, null, 1);
--- ACTUALIZAR PUESTO EN LA TABLA PLAZAPERSONAL.
 
-select l.* from licencia l;
-select bl.* from bitacora_licencia bl;
--- insert into licencia (fechafin, tipolicencia, personal, fechainicio, acta, periodo, anio, congocedesueldo, plaza, subpartida, renglon, motivo, cargo, ubicacion, licencia) 
--- values ();
-select a.* from acuerdo a; 
+SELECT l.* FROM licencia l;
 
--- EXTREAER LA PLAZA ACTUAL DEL DOCENTE.
--- pp.personal, pp.plaza, pp.periodo, pp.anio, pp.subpartida, pp.renglon, p.numero
-SELECT p.plaza, p.numero 
-FROM plaza p 
-WHERE p.plaza in (SELECT p1.plaza 
-                  FROM plazapartida p1 WHERE (p1.plaza, p1.subpartida, p1.renglon) in (SELECT p2.plaza, p2.subpartida, p2.renglon 
-                                                                                       FROM plazapersonal p2 
-                                                                                       WHERE p2.personal = '000018150' AND p2.anio=(EXTRACT(YEAR FROM CURRENT_DATE)-1) AND p2.periodo=(CASE WHEN EXTRACT(MONTH FROM CURRENT_DATE)<=6 THEN 1 ELSE 2 END)));
-
--- REPORTE HOJA RELACION LABORAL.
-SELECT pp.fechainicio, pp.anio, pp.periodo, p.nombre
-FROM plazapersonal pp LEFT JOIN puesto p ON (pp.puesto = p.puesto) 
-WHERE pp.personal = '000018150' 
-ORDER BY pp.anio, pp.periodo;
-
-SELECT EXTRACT(YEAR FROM t.fechaascenso) ANIO, EXTRACT(MONTH FROM t.fechaascenso) MES, t.personal REGISTRO_PERSONAL, p.nombre || ' ' || p.apellido NOMBRE, CASE WHEN t.titular THEN 'TITULAR' ELSE '-'  END CATEGORIA, t.* 
-FROM titularidadhistorial t LEFT JOIN personal p ON (t.personal = p.personal)
-WHERE t.personal = '000006182' 
-ORDER BY ANIO, MES;
